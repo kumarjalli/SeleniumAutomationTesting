9e2b01dbc81ff056ff8421554044c9d177ea2928
kumarjalli <kumar.jalli23@gmail.com>
2019-05-30 01:02:55 +0530

    Add files via upload

---
+++ SeleniumAutomationApplication/NUnit.Core/SampleTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Data;
+using System.Windows.Forms;
+
+namespace NUnit.Core
+{
+    [NUnit.Framework.TestFixture()]
+    public class SampleTest
+    {
+        Selenium.Automation.Form1 form;
+
+        [SetUp]
+        public void SetUp()
+        {
+            form = new Selenium.Automation.Form1();
+            form.Show(null);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            form.Close();
+            form.Dispose();
+        }
+
+        [Test()]
+        public void IsDataTableEmpty()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("CustomerId");
+            dt.Rows.Add(new string[] { "234" });
+            dt.AcceptChanges();
+
+            Assert.AreEqual(((dt == null) || (dt.Rows.Count == 0)), false);
+            dt.Rows.Clear();
+            Assert.AreEqual(((dt == null) || (dt.Rows.Count == 0)), true);
+            dt = null;
+            Assert.AreEqual(((dt == null) || (dt.Rows.Count == 0)), true);
+        }
+
+       
+    }
+}
